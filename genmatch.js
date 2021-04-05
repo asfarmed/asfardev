71e83f1fe74a2e4ff90201d4d38768fbe23b9bf1
asfarmed <asfarmohamed20@gmail.com>
2021-04-05 18:23:50 +0100

    Update genmatch.js

--- genmatch.js
+++ genmatch.js
@@ -27,6 +27,6 @@ $('#textarea1').val('<div class="match-container"> <a data-result="0-0" href="ht
 
 $('#playcode').html('<div class="match-container"> <a data-result="0-0" href="https://ww.online-kora.tv/p/bein-sport-8-tv.html" id="match-live" title="'+datas1+' vs '+datas2+'"> <div class="right-team"> <div class="team-logo"> <img alt="'+datas1+'" height="70" src="'+datas3+'" title="'+datas1+'" width="70"> </div> <div class="team-name">'+datas1+'</div> </div> <div class="match-center"> <div class="match-timing"> <div id="match-time"></div> <div id="result" style="display: none;"></div> <li><span>'+datas5+' </span></li> <div class="date stay mlive" data-gameends="'+datas7+'+02:00" data-start="'+datas8+'+02:00"></div> </div> </div> <div class="left-team"> <div class="team-logo"> <img alt="'+datas2+' " height="70" src="'+datas4+'" title="'+datas2+' " width="70"> </div> <div class="team-name">'+datas2+' </div> </div> <div id="overlay-match"><div id="watch-match"></div></div> </a> </div>');
 
-
+playtime();
 });
     
